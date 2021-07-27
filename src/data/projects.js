6b284b90ad0f57597b39b7c21316345cbab62558
avinashkumarsingh15 <avihunny1598@gmail.com>
2021-07-27 23:45:22 +0530

    first react commit

---
+++ src/data/projects.js
@@ -0,0 +1,20 @@
+export const ProjectData=[
+    {
+        id:1,
+        title:"Online library management",
+        about:"Project on Online library management system using HTML5,CSS and JavaScript. which have two logins, one for librarian and other for students",
+        tags:["HTML5","CSS","Javascript"],
+        demo:'xyz',
+        github:'www.github.com/Avinashkumarsingh15',
+        image:require('../assets/library.png').default,
+    },
+    {
+        id:1,
+        title:"Cyber Bullying detection",
+        about:"Cyber Bullying detection using semantic marginalised stacked denoising autoencoder from myspace and twitter data set. Python tkinter for GUI",
+        tags:["Python","Machine Learning","OpenCV","GCP"],
+        demo:'xyz',
+        github:'www.github.com/Avinashkumarsingh15',
+        image:require('../assets/cyberbully.PNG').default,
+    },
+]
---
+++ src/data/work.js
@@ -0,0 +1,13 @@
+export const WorkData = [
+    {
+      company: "Infosys",
+      designation: "Systems Engineer",
+      dateJoining: "03-03-2021",
+      dateEnd: "Present",
+      companyLogo:
+        require('../assets/infosys.png').default,
+      work: "Working as Java Full Stack developer. ",
+    },
+    
+  ];
+  
